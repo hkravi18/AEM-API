efff5c1e719d1026e11e63f805476ad33405088a
harshit kumar ravi <hkravi2002@gmail.com>
2024-03-15 00:38:21 +0530

    Change configuration files of linting tools

--- api/index.js
+++ api/index.js
@@ -1,26 +1,27 @@
-require('dotenv').config();
-const app = require('./app.js');
+require("dotenv").config();
+const app = require("./app.js");
 
-//port 
+//port
 const port = process.env.PORT || 4000;
 
-//middlewares 
-const logger = require('./src/middleware/loggerMiddleware.js');
-const errorHandler = require('./src/middleware/errorHandler.js');
+//middlewares
+const logger = require("./src/middleware/loggerMiddleware.js");
+const errorHandler = require("./src/middleware/errorHandler.js");
 
-//importing routes 
-const authRoutes = require('./src/routes/authRoutes.js');
-const transactionRoutes = require('./src/routes/transactionRoutes.js');
+//importing routes
+const authRoutes = require("./src/routes/authRoutes.js");
+const transactionRoutes = require("./src/routes/transactionRoutes.js");
 
 //logger middleware
 app.use(logger);
 
-//routes 
-app.use('/api/auth', authRoutes);
-app.use('/api/transactions', transactionRoutes);
+//routes
+app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionRoutes);
 
+//custom error handler
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server listening on PORT: ${port}`);
+  console.log(`Server listening on PORT: ${port}`);
 });
